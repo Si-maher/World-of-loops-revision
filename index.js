3933a0bda06a59b3272da58d38b58b24a4021784
Simon Maher <simonmaher8307@hotmail.com>
2020-01-14 15:08:43 +0200

    added totalExamScore to print the total

--- index.js
+++ index.js
@@ -12,7 +12,14 @@ for ( i = 0; i < examScores.length; i++){
 
 const reverseExamScores = [98,77,84,91,57,66]
     for (let i = reverseExamScores.length -1; i >=0; i--){console.log(i, reverseExamScores[i]);
-    
-
     }
+    
+let total = 0
+const totalExamScores = [98,77,84,91,57,66]
+for ( let i = 0; i < totalExamScores.length; i++) {
+ total += totalExamScores[i];
+}
+ console.log(total);
+ 
+
 
