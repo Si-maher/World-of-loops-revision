12f8c0ae7d77507fbd7956ebd95bbe59666d726c
Simon Maher <simonmaher8307@hotmail.com>
2020-01-14 14:55:22 +0200

    added examScore array to print items out

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ for (let i = 0; i <animals.length; i++){
     console.log(i,animals[i]);
     
 }
+
+const examScores = [98,77,84,91,57,66]
+for ( i = 0; i < examScores.length; i++){
+    console.log(i,examScores[i]);
+    
+}
