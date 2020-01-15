c9cb5d08b2c00f8173f3756d249507ecbb00830b
Simon Maher <simonmaher8307@hotmail.com>
2020-01-15 16:38:49 +0200

    created nested for loop for the gameBoard

--- index.js
+++ index.js
@@ -77,3 +77,11 @@ for (let i = 0; i <= 5; i ++){
       [8,32,16,4],
       [2,8,4,2],
   ]  
+  let totalScore = 0
+  for (let i = 0; i < gameBoard.length; i ++) {
+      let row = gameBoard[i]
+      for ( let j = 0; j < row.length; j ++)
+      totalScore += row[j]
+      console.log(totalScore);
+      
+  }
