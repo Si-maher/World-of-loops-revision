97d20e0b0aa8657fa0062547e207cd8d150d5fe6
Simon Maher <simonmaher8307@hotmail.com>
2020-01-17 14:03:06 +0200

    added for loop to create words from two arrays

--- index.js
+++ index.js
@@ -146,3 +146,11 @@ for (let row of magicSquare){
 
 
 // ######################################################
+
+const words1 = ['mail','milk','bath','black']
+const words2 = ['box', 'shake', 'tub', 'berry']
+
+for (let i = 0; i < words1.length; i ++) {
+    console.log(`${words1[i]}${words2[i]}`);
+    
+}
